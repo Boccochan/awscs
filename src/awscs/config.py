ca09810d5ee9b26e0b8a6764ce93972ab9907c7f
Boccochan <boccochan@gmail.com>
2019-12-01 23:45:25 +0900

    Delete comment out

--- src/awscs/config.py
+++ src/awscs/config.py
@@ -2,10 +2,10 @@ import os
 import json
 from pathlib import Path
 
-from typing import Dict, Optional, List
-
+from typing import Dict, Optional
 
 DEFAULT_PATH = os.path.join(str(Path.home()), '.aws','.config.json')
+
 
 def get_setting() -> Optional[Dict[str, str]]:
     try:
@@ -21,12 +21,3 @@ def get_setting() -> Optional[Dict[str, str]]:
 def set_setting(profile: str, region: str):
     with open(os.getenv('AWSCSPATH', DEFAULT_PATH), 'w') as f:
         json.dump({"AWS_PROFILE": profile, "AWS_DEFAULT_REGION": region}, f)
-
-
-# def load_regions() -> List[str]:
-#     # currently, regions list is fixed.
-#     return [
-#         'ap-northeast-1',
-#         'us-east-1',
-#         'ap-southeast-1',
-#     ]
